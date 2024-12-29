d079b83842efb838bb9bef58e8609e3d817c439b
pkay025 <nunoomensahemmanuel224@gmail.com>
2024-12-29 07:24:57 +0000

    initial commit

--- frontend/src/components/Navbar/Hero/Hero.jsx
+++ frontend/src/components/Navbar/Hero/Hero.jsx
@@ -1,33 +1,15 @@
 import React from "react";
-import Image1 from "../../../assets/hero/headphone.png";
-import Image2 from "../../../assets/category/vr.png";
-import Image3 from "../../../assets/category/macbook.png";
+import Image1 from "../../../assets/product/1.png";
+import Image2 from "../../../assets/category/gaming.png";
+import Image3 from "../../../assets/product/r-5.png"; 
 import Slider from "react-slick";
 
 const ImageList = [
-  {
-    id: 1,
-        img: Image1,
-        title: "Upto 50% off for ",
-        
-    },
-  {
-    id: 2,
-        img: Image2,
-        subtitle: "Beats Solo",
-        title: "Wireless",
-        title2: "Virtual Reality",
-  },
-  {
-    id: 3,
-        img: Image3,
-        subtitle: "Beats Solo",
-        title: "Branded",
-        title2: "Laptops",
-  },
-];
 
-const Hero = ({ handleOrderPopup }) => {
+ 
+]
+
+const Hero = () => {
   var settings = {
     dots: false,
     arrows: false,
@@ -42,67 +24,63 @@ const Hero = ({ handleOrderPopup }) => {
   };
 
   return (
-    <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 ">
+    <div className="relative overflow-hidden min-h-[550px]
+    sm:min-h-[650px] bg-gray-100 flex justify-center
+    items-center dark:bg-gray-950 dark:text-white
+    duration-200">
       {/* background pattern */}
-      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]"></div>
+      <div className="h-[700px] w-[700px] bg-primary/20 
+      absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-10">
+      </div>
       {/* hero section */}
       <div className="container pb-8 sm:pb-0">
         <Slider {...settings}>
-          {ImageList.map((data) => (
-            <div>
-              <div className="grid grid-cols-1 sm:grid-cols-2">
-                {/* text content section */}
-                <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
-                  <h1
-                    data-aos="zoom-out"
-                    data-aos-duration="500"
-                    data-aos-once="true"
-                    className="text-5xl sm:text-6xl lg:text-7xl font-bold"
-                  >
+            {ImageList.map((data) => (
+                <div>
+                <div className="grid grid-cols-1 sm:grid-cols-2">
+                  {/* text content section */}
+                  <div className="flex flex-col justify-center gap-4 pt-12 
+                  sm:pt-0 text-center sm:text-left order-2 
+                  sm:order-1 relative z-10">
+                  <h1 className="text-5xl sm:text-6xl lg:text-7xl
+                   font-bold">
                     {data.title}
-                  </h1>
-                  <p
-                    data-aos="fade-up"
-                    data-aos-duration="500"
-                    data-aos-delay="100"
-                    className="text-sm"
-                  >
+                   </h1>
+                   <p className="text-sm">
                     {data.description}
-                  </p>
-                  <div
-                    data-aos="fade-up"
-                    data-aos-duration="500"
-                    data-aos-delay="300"
-                  >
-                    <button
+                   </p>
+          
+                  <div>
+                      <button
                       onClick={handleOrderPopup}
-                      className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full"
-                    >
-                      Order Now
-                    </button>
+                      className="bg-gradient-to-r from-secondary
+                      to-tertiary hover:scale-105 
+                      duration-200 text-white py-2 px-4
+                      rounded-full"
+                      >
+                        Order Now
+                      </button>
+                      </div>
                   </div>
-                </div>
-                {/* image section */}
-                <div className="order-1 sm:order-2">
-                  <div
-                    data-aos="zoom-in"
-                    data-aos-once="true"
-                    className="relative z-10"
-                  >
-                    <img
-                      src={data.img}
-                      alt=""
-                      className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto"
+                  {/* image section */}
+                  <div className="order-1 sm:order-2">
+                      <div className="relative z-10">
+                      <img src={data.img} alt=""
+                      className="w-[300px] h-[300px] sm:h-[450px] 
+                      sm:w-[450px] sm:scale-125 lg:scale-120 
+                      object-contain mx-auto"
                     />
                   </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </Slider>
-      </div>
+              </div>    
+          </div>    
+          </div>
+            ))}
+        
+ </Slider>
+    </div>
     </div>
   );
 };
 
 export default Hero;
+  
